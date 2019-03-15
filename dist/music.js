5311ea3411219ef7042c752071f59464c9abb1a7
CainChao <sung1114chao@163.com>
2019-03-16 00:08:30 +0800

    Site updated: 2019-03-16 00:08:30

---
+++ dist/music.js
@@ -0,0 +1,49 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "The Day You Went Away",
+        artist: 'M2M',
+        url: 'http://www.ytmp3.cn/down/44343.mp3',
+        cover: 'http://p1.music.126.net/pETQnye5ooWtzXSCC0qbeQ==/1744924953286126.jpg?param=130y130',
+      },
+      {
+        name: "生僻字",
+        artist: '陈柯宇',
+        url: 'http://www.ytmp3.cn/down/56174.mp3',
+        cover: 'http://p1.music.126.net/fzy5I3GvAjiDfwhIEbgXuw==/109951163062323125.jpg?param=130y130',
+      },
+      {
+        name: "浪子回头",
+        artist: '茄子蛋',
+        url: 'http://www.ytmp3.cn/down/57545.mp3',
+        cover: 'http://p1.music.126.net/emWwYFceRZ2plNWgnGUDfg==/109951163333175426.jpg?param=130y130',
+      },
+      {
+        name: "盗将行",
+        artist: '包我也',
+        url: 'http://www.ytmp3.cn/down/57446.mp3',
+        cover: 'http://p1.music.126.net/dio5DBc-JzfTQE_0HrLu-Q==/109951163388237107.jpg?param=130y130',
+      },
+      {
+        name: 'Fade',
+        artist: 'Alan Walker',
+        url: 'http://www.ytmp3.cn/down/35843.mp3',
+        cover: 'http://p2.music.126.net/6Skxbgl8cfIjFGV-MzGi0Q==/109951163787691327.jpg?param=130y130',
+      },
+      {
+        name: '平凡之路',
+        artist: '朴树',
+        url: 'http://www.ytmp3.cn/down/48823.mp3',
+        cover: 'http://p2.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+      },
+	  {
+        name: 'Wonderful U (Demo Version)',
+        artist: 'AGA',
+        url: 'http://www.ytmp3.cn/down/51181.mp3',
+        cover: 'http://p1.music.126.net/UIE3T_txKzd6e8PORxmpCQ==/109951163412959613.jpg?param=130y130',
+      }
+    ]
+});
